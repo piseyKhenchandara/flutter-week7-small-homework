3d4fadbeb6744ebb274a1c22bc5cb8c00e491414
Khen chandarapisey <piseykhenchandara93@gmail.com>
2026-03-04 22:36:58 +0700

    Create two new classes

---
+++ ui/screens/library/widgets/library_content.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class LibraryContent extends StatelessWidget {
+  const LibraryContent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
